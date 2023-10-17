5dbfce3c149427188da4a25d2f07e0ed3855c747
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-10-17 17:26:01 -0500

    [nc] correct shellcheck issues in install_terraform.sh
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_terraform.sh
+++ src/scripts/install_terraform.sh
@@ -2,25 +2,21 @@
 
 # download and import HashiCorp public key
 curl --remote-name https://keybase.io/hashicorp/pgp_keys.asc
-result=$(gpg --import pgp_keys.asc 2>&1)
-
-if [ $? -eq 0 ]; then
-    echo "loaded HashiCorp public key"
+if result=$(gpg --import pgp_keys.asc 2>&1); then
+  echo "loaded HashiCorp public key"
 else
     echo "Unable to import HashiCorp key"
     echo "GPG Output:"
     echo "$result"
 fi
 
-# download terraform installation package, SHA checksum and signatures
-curl --remote-name https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-curl --remote-name https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS
-curl --remote-name https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig
+# download terraform installation packages
+curl --remote-name "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+curl --remote-name "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS"
+curl --remote-name "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig"
 
 # verify SHA signature
-result=$(gpg --verify terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig terraform_${TERRAFORM_VERSION}_SHA256SUMS 2>&1)
-
-if [ $? -eq 0 ]; then
+if result=$(gpg --verify "terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig terraform_${TERRAFORM_VERSION}_SHA256SUMS" 2>&1); then
     echo "Verified SHA signature"
 else
     echo "Unable to verify SHA signature"
@@ -29,10 +25,8 @@ else
     exit 1
 fi
 
-# verify terraform cli download
-result=$(shasum --ignore-missing --algorithm 256 --check terraform_${TERRAFORM_VERSION}_SHA256SUMS 2>&1)
-
-if [ $? -eq 0 ]; then
+# verify terraform cli package
+if result=$(shasum --ignore-missing --algorithm 256 --check "terraform_${TERRAFORM_VERSION}_SHA256SUMS" 2>&1); then
     echo "Verified terraform package"
 else
     echo "Unable to terraform package"
@@ -41,5 +35,6 @@ else
     exit 1
 fi
 
+# install and smoke test terraform cli
 sudo unzip -o "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -d /usr/local/bin
 terraform version
