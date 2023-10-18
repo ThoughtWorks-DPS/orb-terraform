dc83a8a6a599477f80f154481383c88896f450e8
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-10-18 00:13:38 -0500

    [nc] shellcheck edits
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_trivy.sh
+++ src/scripts/install_trivy.sh
@@ -6,7 +6,7 @@ curl -SLO "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERS
 curl -SLO "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.sig"
 
 # verify signature
-if result=$(cosign verify-blob trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz --certificate trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.pem --signature trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.sig --certificate-identity-regexp 'https://github\.com/aquasecurity/trivy/\.github/workflows/.+' --certificate-oidc-issuer "https://token.actions.githubusercontent.com" 2>&1); then
+if result=$(cosign verify-blob "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz" --certificate "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.pem" --signature "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.sig" --certificate-identity-regexp 'https://github\.com/aquasecurity/trivy/\.github/workflows/.+' --certificate-oidc-issuer "https://token.actions.githubusercontent.com" 2>&1); then
     echo "Verified trivy installation package"
 else
     echo "Unable to verify trivy installation package"
@@ -16,6 +16,6 @@ else
 fi
 
 # install and smoke test trivy
-tar -xzf trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz
+tar -xzf "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz"
 sudo mv trivy /usr/local/bin/trivy
 trivy --version
