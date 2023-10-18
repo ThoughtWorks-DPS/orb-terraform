d07580849ca9b06f5d65e00ec7b73eb948f605b9
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-10-18 14:11:24 -0500

    [nc] add terrascan to packages
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ src/scripts/install_terrascan.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# download terrascan and verification packages
+curl -SLO "https://github.com/tenable/terrascan/releases/download/v${TERRASCAN_VERSION}/checksums.txt"
+curl -SLO "https://github.com/tenable/terrascan/releases/download/v${TERRASCAN_VERSION}/terrascan_${TERRASCAN_VERSION}_Linux_x86_64.tar.gz"
+
+# verify terrascan package sha
+if result=$(shasum --ignore-missing --algorithm 256 --check  checksums.txt 2>&1); then
+    echo "Verified terrascan package sha"
+else
+    echo "Unable to verify terrascan package sha"
+    echo "shasum Output:"
+    echo "$result"
+    exit 1
+fi
+
+tar -xf terrascan_${TERRASCAN_VERSION}_Linux_x86_64.tar.gz terrascan
+sudo mv terrascan /usr/local/bin/terrascan
+terrascan version
