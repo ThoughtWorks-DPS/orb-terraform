0c2962383999949faf90ca3b154ad7673db29c19
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-10-19 16:48:52 -0500

    [nc] static terrascan test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/variables.tf
+++ test/variables.tf
@@ -10,7 +10,7 @@ variable "aws_account_id" {
   type = string
   validation {
     condition     = length(var.aws_account_id) == 12 && can(regex("^\\d{12}$", var.aws_account_id))
-    error_message = "Invalid AWS account ID"
+    error_message = "Invalid AWS account ID."
   }
 }
 
@@ -25,7 +25,7 @@ variable "vpc_cidr" {
   type = string
   validation {
     condition     = can(cidrhost(var.vpc_cidr, 32))
-    error_message = "Invalid IPv4 CIDR"
+    error_message = "Invalid IPv4 CIDR."
   }
 }
 
@@ -37,12 +37,12 @@ variable "vpc_azs" {
     condition = alltrue([
       for v in var.vpc_azs : can(regex("[a-z][a-z]-[a-z]+-[1-9][a-c]", v))
     ])
-    error_message = "Invalid VPC AZ name"
+    error_message = "Invalid VPC AZ name."
   }
 
   validation {
     condition     = length(var.vpc_azs) == 3
-    error_message = "length of list(string) not equal to 3 "
+    error_message = "Length of list(string) not equal to 3."
   }
 }
 
@@ -54,12 +54,12 @@ variable "vpc_private_subnets" {
     condition = alltrue([
       for v in var.vpc_private_subnets : can(cidrhost(v, 32))
     ])
-    error_message = "Invalid IPv4 CIDR"
+    error_message = "Invalid IPv4 CIDR."
   }
 
   validation {
     condition     = length(var.vpc_private_subnets) == 3
-    error_message = "length of list(string) not equal to 3 "
+    error_message = "Length of list(string) not equal to 3."
   }
 }
 
@@ -71,12 +71,12 @@ variable "vpc_public_subnets" {
     condition = alltrue([
       for v in var.vpc_public_subnets : can(cidrhost(v, 32))
     ])
-    error_message = "Invalid IPv4 CIDR"
+    error_message = "Invalid IPv4 CIDR."
   }
 
   validation {
     condition     = length(var.vpc_public_subnets) == 3
-    error_message = "length of list(string) not equal to 3 "
+    error_message = "Length of list(string) not equal to 3."
   }
 }
 
@@ -88,11 +88,11 @@ variable "vpc_database_subnets" {
     condition = alltrue([
       for v in var.vpc_database_subnets : can(cidrhost(v, 32))
     ])
-    error_message = "Invalid IPv4 CIDR"
+    error_message = "Invalid IPv4 CIDR."
   }
 
   validation {
     condition     = length(var.vpc_database_subnets) == 3
-    error_message = "length of list(string) not equal to 3 "
+    error_message = "Length of list(string) not equal to 3."
   }
 }
