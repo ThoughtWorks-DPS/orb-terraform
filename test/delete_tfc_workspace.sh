d19abad53e99a06492b255cdc82b29a3498c6e9f
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-11-03 16:59:44 -0500

    [nc] test plan job
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/delete_tfc_workspace.sh
+++ test/delete_tfc_workspace.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+TFC_ORGANIZATION=twdps
+TFC_WORKSPACE=orb-terraform-di
+
 # setup curl parameters
 tfc_api_url="https://app.terraform.io/api/v2/organizations"
 tfc_api_url_workspace_by_name="$tfc_api_url/$TFC_ORGANIZATION/workspaces/$TFC_WORKSPACE"
@@ -9,7 +12,8 @@ headers=(
   "Content-Type: application/vnd.api+json"
 )
 echo "deleteing workspace $TFC_WORKSPACE"
-createworkspace=$(curl --header "${headers[0]}" \
+deleteworkspace=$(curl --header "${headers[0]}" \
                         --header "${headers[1]}" \
                         --request DELETE \
                         "$tfc_api_url_workspace_by_name")
+echo $deleteworkspace
