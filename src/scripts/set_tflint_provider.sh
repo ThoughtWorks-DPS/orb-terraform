ea785e744a915760e8a70ef5a79db21eb194a7aa
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-01-22 18:15:25 -0600

    [nc] adjusting tflint plugin install method
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ src/scripts/set_tflint_provider.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [[ ! -f ".tflint.hcl" ]]; then
+  export ver=$(curl  "https://api.github.com/repos/terraform-linters/tflint-ruleset-$PROVIDER/tags" | jq -r '.[0].name' | grep -Eo '[0-9]\.[0-9]+\.[0-9]+')
+  cat \<<EOF > .tflint.hcl
+plugin "aws" {
+    enabled = true
+    version = "$ver"
+    source  = "github.com/terraform-linters/tflint-ruleset-$PROVIDER"
+}
+EOF
+else
+  echo "Provider not defined because .tflint.hcl already exists in working directory"
+fi
