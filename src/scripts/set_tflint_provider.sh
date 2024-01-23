244680b9d0fd97c92a0f501a8634f38ac55a61ca
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-01-22 18:21:19 -0600

    [nc] adjusting tflint plugin install method
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/set_tflint_provider.sh
+++ src/scripts/set_tflint_provider.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 if [[ ! -f ".tflint.hcl" ]]; then
   export ver=$(curl  "https://api.github.com/repos/terraform-linters/tflint-ruleset-$PROVIDER/tags" | jq -r '.[0].name' | grep -Eo '[0-9]\.[0-9]+\.[0-9]+')
-  cat \<<EOF > .tflint.hcl
+  cat <<EOF > .tflint.hcl
 plugin "aws" {
     enabled = true
     version = "$ver"
